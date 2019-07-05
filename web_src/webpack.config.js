3c934ecd956b46fd544a63ea622369436f603d9c
marvin1311 <marvin1311@foxmail.com>
2019-07-05 16:51:17 +0800

    modify CleanWebpackPlugin path

--- web_src/webpack.config.js
+++ web_src/webpack.config.js
@@ -160,7 +160,7 @@ module.exports = {
 if (process.env.NODE_ENV == "production") {
     module.exports.plugins = (module.exports.plugins || []).concat([
         new CleanWebpackPlugin(['www'], {
-            root: resolve("../")
+            root: resolve("../../")
         }),
         new webpack.DefinePlugin({
             'process.env': {
