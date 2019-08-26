d36785abec7dc38ed0c702b013e23d1c9e2b0232
penggy <peng.wu@foxmail.com>
2019-08-26 16:46:53 +0800

    fix screen play

--- web_src/components/Screen.vue
+++ web_src/components/Screen.vue
@@ -131,9 +131,9 @@ export default {
       }).then(stream => {
         var videoUrl = this.isMobile() ? stream.HLS : stream.RTMP;
         if(this.flvSupported()) {
-          if(index < 6 && stream.FLV) {
+          if(index < 4 && stream.FLV) {
             videoUrl = stream.FLV;
-          } else if (index >= 6 && stream.WS_FLV) {
+          } else if (index >= 4 && stream.WS_FLV) {
             videoUrl = stream.WS_FLV;
           }
         }
